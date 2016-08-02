eb736442ca449759cf20e5caf32ad060e771e4c7
zeusmedina <zeusmedina2@gmail.com>
2016-08-01 19:46:40 -0700

    Moved functions from MovieRanker into WikiMovie for maximum OOP

--- javacs-lab12/src/com/flatironschool/javacs/WikiMovie.java
+++ javacs-lab12/src/com/flatironschool/javacs/WikiMovie.java
@@ -1,7 +1,13 @@
 package com.flatironschool.javacs;
 
-import redis.clients.jedis.Jedis;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
 import java.net.URL;
+
+import redis.clients.jedis.Jedis;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -11,91 +17,114 @@ import org.jsoup.select.Elements;
 
 public class WikiMovie {
 
-	// Global Variables
+  // Global Variables
 
-	public String url;
+  public String url;
 
-	public String directorUrl;
-	public List<String> castUrls;
+  public String directorUrl;
+  public List<String> castUrls;
 
-	public String rottenTomatoesScore;
-	public String metaCriticScore;
+  public String rottenTomatoesScore;
+  public String metaCriticScore;
+  public static String  wikiBaseUrl = "https://en.wikipedia.org";
+  public int [directorAwardCount] = 0;
 
-	// Constructors
+  // Constructors
 
-	public WikiMovie(String url) {
-		this.url = url;
-		init();
-	}
+  public WikiMovie(String url) throws IOException{
+    this.url = url;
+    init();
+  }
 
-	// Methods
+  // Methods
 
-	public void init() throws IOException {
+  public void init() throws IOException {
 
-		// download and parse the document
-		Document doc = Jsoup.connect(wikiUrl).get();
+    // download and parse the document
+    // Document doc = Jsoup.connect(wikiUrl).get();
 
-		// select the main content text 
-		Element content = doc.getElementById("mw-content-text");
+    // select the main content text
+    // Element content = doc.getElementById("mw-content-text");
+    directorUrl = getDirectorWikiUrl(url);
+    directorAwardCount = getDirectorAwardCount(url);
 
-		directorUrl = getDirectorUrl(content);
-		castUrls = getCastUrls(content);
+    /*castUrls = getCastUrls(content);
+    rottenTomatoesScore = getRottenTomatoesScore(content);
+    metaCriticScore = getMetaCriticScore(content);*/
+  }
 
-		rottenTomatoesScore = getRottenTomatoesScore(content);
-		metaCriticScore = getMetaCriticScore(content);
-	}
+  /*// return a List of URLs of the WikiMovie's cast
+  public List<String> getCastUrls(Element content) {
 
-  	// return the URL of the WikiMovie's director
-	public String getDirectorUrl(Element content) {
+    // The list to return
+    List<String> castList = new ArrayList<>();
 
-	    // Table found on right hand side of movie wiki page
-	    Element movieInfoTable = content.select(".infobox.vevent").first();
+    return castList;
+  }
 
-	    // All the rows inside of the table
-	    Elements rows = movieInfoTable.select("tr");
+  // return the Rotten Tomatoes score of the movie
+  public String getRottenTomatoesScore(Element content) {
 
-	    // finds director url
-	    String directorWikiUrl = rows.get(2).select("td").select("a").first().attr("href");
+    String score = null; // the Rotten Tomatoes score
 
-	    return directorWikiUrl;
-  	}
+    Elements paragraphs = content.select("p");
 
-  	// return a List of URLs of the WikiMovie's cast
-  	public List<String> getCastUrls(Element content) {
+    for (Element p : paragraphs) { // iterate the paragraphs
 
-  		// The list to return
-  		List<String> castList = new ArrayList<>();
+      String textContent = p.html().toLowerCase();
 
-  		return castList;
-  	}
+      if (textContent.contains("rotten tomatoes")) {
 
-  	// return the Rotten Tomatoes score of the movie
-  	public String getRottenTomatoesScore(Element content) {
+        int targetSentenceIndex = textContent.indexOf("rotten tomatoes");
+        String targetSentence = getContainingSentence(textContent, targetSentenceIndex);
 
-  		String score = null; // the Rotten Tomatoes score
+        break;
+      }
+    }
 
-  		Elements paragraphs = content.select("p");
+    return score;
+  }
 
-  		for(Element p : paragraphs) { // iterate the paragraphs
-
-  			String textContent = p.html().toLowerCase();
-
-  			if(textContent.contains("rotten tomatoes")) {
-
-  				int targetSentenceIndex = textContent.indexOf("rotten tomatoes");
-  				String targetSentence = getContainingSentence(textContent,targetSentenceIndex);
-
-  				break;
-  			}
-  		}
-
-  		return score;
-  	}
-
-  	// returns the percent number closest to the index given in a block of text restricted to the sentence containing the index
-  	private String getContainingSentence(String textContent, int targetSentenceIndex) {
+  // returns the percent number closest to the index given in a block of text restricted to the sentence containing the index
+  private String getContainingSentence(String textContent, int targetSentenceIndex) {
 
 
-  	}
+  }*/
+
+  public static String getDirectorWikiUrl(String source) throws IOException {
+    // Grabs the Movie URL for use in JSoup
+    Document doc = Jsoup.connect(source).get();
+    // Table found on right hand side of movie wiki page
+    Elements movieInfoTable = doc.select(".infobox.vevent");
+    // All the rows inside of the table
+    Elements rows = movieInfoTable.select("tr");
+    // finds director url and appends to base url
+    String directorWikiUrl = wikiBaseUrl + rows.get(2).select("td").select("a").first().attr("href");
+    return directorWikiUrl;
+  }
+
+  public static int getDirectorAwardCount(String source) throws IOException {
+    Document doc = Jsoup.connect(getDirectorWikiUrl(source)).get();
+    // Given that there is no common format for Director accolades.
+    // This code counts particular words that would be expected in 'successful' director page
+    // Hypothesis: Higher  frequency of these words -> higher chance of winning oscar
+    Elements won = doc.getElementsContainingOwnText(" won ");
+    Elements popular = doc.getElementsContainingOwnText(" popular ");
+    Elements influential = doc.getElementsContainingOwnText(" influential ");
+    Elements award = doc.getElementsContainingOwnText(" award ");
+    Elements acclaimed = doc.getElementsContainingOwnText(" acclaimed ");
+    Elements historic = doc.getElementsContainingOwnText(" historic ");
+    Elements festival = doc.getElementsContainingOwnText(" festival ");
+    Elements nominated = doc.getElementsContainingOwnText(" nominated ");
+    int sumOfTerms = won.size() + popular.size() + influential.size() + award.size()
+                     + acclaimed.size() + festival.size() + historic.size() + nominated.size();
+    return sumOfTerms;
+  }
+
+    public static boolean releasedDuringOscarSeason(String source) throws IOException {
+    // Checks to see if a movie was released during September - December. If so, return true.
+    return false;
+  }
+
 
 }
